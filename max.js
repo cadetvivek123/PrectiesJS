473749bf6517f24bbd2dcb7fbd4a74c5e36f6fb2
cadetvivek123 <vivek.kushwah@blostem.com>
2025-06-13 10:42:01 +0530

    first commit

---
+++ max.js
@@ -0,0 +1,16 @@
+
+function max(arr) {
+  if (arr.length === 0) {
+    return null; // Handle empty array case
+  }
+  let maxValue = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxValue) {
+      maxValue = arr[i];
+    }
+  }
+  return maxValue;
+}
+
+let arr = [1, 2, 3, 4, 5];
+console.log(max(arr))
